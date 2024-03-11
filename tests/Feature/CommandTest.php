4251490d63030bb4157b6a7cd16e6434f13c4a33
SlavikRacoon <lacostefracture@gmail.com>
2024-03-11 14:45:00 +0100

    Test tutorial

---
+++ tests/Feature/CommandTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CommandTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_command_make_user(): void
+    {
+        $this->artisan('user:make')
+        ->assertSuccessful();
+    }
+}
