ec6432a068ac86b90445cee15a911584dffbb275
viacheslav <lacostefracture@gmail.com>
2024-04-18 18:21:14 +0200

    Add_assert

--- tests/Feature/UserPageTest.php
+++ tests/Feature/UserPageTest.php
@@ -21,6 +21,7 @@ class UserPageTest extends TestCase
         $response->assertStatus(200);
 
         $response->assertJsonPath('name', 'Test');
+        $response->assertJsonPath('email', 'test@test.com');
 
     }
 }
